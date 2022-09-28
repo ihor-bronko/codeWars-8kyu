51d6b11baa28d6898a3df8cfbbf25667559e8637
Suric1 <igorbronko@gmail.com>
2022-09-29 00:16:16 +0300

    Create Find the position!

---
+++ Find the position.js
@@ -0,0 +1,9 @@
+// Find the position!
+
+
+const position = letter => `Position of alphabet: ${letter.charCodeAt() -96}`
+
+
+console.log(position("a"));
+console.log(position("z"));
+console.log(position("e"));
