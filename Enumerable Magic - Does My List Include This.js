c2f66765a2ff9b5f4a49c0b4d2247d1987f26ee7
Suric1 <igorbronko@gmail.com>
2022-10-13 13:01:49 +0300

    Create Enumerable Magic - Does My List Include This

--- Enumerable Magic - Does My List Include This.js
+++ Enumerable Magic - Does My List Include This.js
@@ -1,9 +1,4 @@
 // Enumerable Magic - Does My List Include This?
 
-function include(arr, item){
-	// ...
-}
+const include = (arr, item) => arr.includes(item)
 
-
-console.log(include([1,2,3,4], 3))
-console.log(include([1,2,4,5], 3))
